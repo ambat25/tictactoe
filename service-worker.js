40d8a96d3482b725fc32e849a4777168cd9fb9de
Abdulrahman <25ambat25@gmail.com>
2018-06-28 17:40:12 +0100

    updated service worker

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,22 @@
-let cacheName = 'tictactoe-v1';
+let cacheName = 'tictactoe-v9';
 self.addEventListener('install', event => {
+  // console.log({event});
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => {
         return cache.addAll([
-          '/index.html',
-          '/assets/css/main.css',
-          '/assets/js/app.js'
+          `${location.origin}/index.html`,
+          `${location.origin}/assets/css/main.css`,
+          `${location.origin}/assets/js/app.js`,
+          `${location.origin}/manifest/manifest.json`,
+          `${location.origin}/manifest/images/icons/icon-72x72.png`,
+          `${location.origin}/manifest/images/icons/icon-96x96.png`,
+          `${location.origin}/manifest/images/icons/icon-128x128.png`,
+          `${location.origin}/manifest/images/icons/icon-144x144.png`,
+          `${location.origin}/manifest/images/icons/icon-152x152.png`,
+          `${location.origin}/manifest/images/icons/icon-192x192.png`,
+          `${location.origin}/manifest/images/icons/icon-384x384.png`,
+          `${location.origin}/manifest/images/icons/icon-512x512.png`,
         ])
           .then(() => self.skipWaiting());
       })
